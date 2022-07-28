995c408556cf6ac496b9a52151ad080458e023d7
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-07-28 21:33:12 +0100

    Adding the adapter

--- src/components/app.test.js
+++ src/components/app.test.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Adapter from 'enzyme-adapter-react-18'
 import { shallow } from 'enzyme'
 import App from './App'
 
 const app = shallow(<App />)
+configure({ adapter: new Adapter() })
 
 it('renders correctly', () => {
 	expect(app).tpMatchSnapshot()
